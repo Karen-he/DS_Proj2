f0c015c8022cae3025b2d8ddd3b6ef3fe5e77004
PURPLE-YO <31911853+PURPLE-YO@users.noreply.github.com>
2017-10-18 03:53:13 +1100

    update user sys and userServant in server

--- src/RMIInterfaces/UserSysInterface.java
+++ src/RMIInterfaces/UserSysInterface.java
@@ -1,6 +1,12 @@
 package RMIInterfaces;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface UserSysInterface extends Remote {
+    boolean listenRequestList() throws RemoteException;
+    void addApprove(String userName, boolean approve) throws RemoteException;
+    void sendRequest(String userName) throws RemoteException;
+    int checkApproval(String userName) throws RemoteException;
+
 }
