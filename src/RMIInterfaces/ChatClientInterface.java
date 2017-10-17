be5b976c3b906da22392f6db8b7720ac8fb8153f
PURPLE-YO <31911853+PURPLE-YO@users.noreply.github.com>
2017-10-17 11:01:51 +1100

    ChatPrint

--- src/RMIInterfaces/ChatClientInterface.java
+++ src/RMIInterfaces/ChatClientInterface.java
@@ -1,8 +1,11 @@
 package RMIInterfaces;
 
+import Client.WBController;
+
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface ChatClientInterface extends Remote {
     void retrieveMsg(String chatContent) throws RemoteException;
+
 }
